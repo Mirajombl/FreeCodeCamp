db817c3dfd78eb62ca05ec3ade038a6b8999fb26
Jonathan Fitzsimons <Jonathan.sfitzsimons@outlook.com>
2026-03-12 13:53:56 +1300

    free code camp progress

--- Var-DataType/let char = String.js
+++ Var-DataType/let char = String.js
@@ -1,2 +1,20 @@
-let char = String.fromCharCode();
-console.log(char);  // a
+console.log(String.fromCharCode(66));
+
+let text = "Hello, JavaScript world!";
+let result = text.includes("JavaScript", 7);
+
+console.log(result);  // true
+
+let message = "JavaScript is great!";
+let result2 = message.includes("script");
+console.log(result2);
+
+let text2 = "JavaScript is awesome!";
+let result3 = text2.slice(0, 9);
+
+console.log(result3);
+
+let sentence = "Learning JavaScript is fun!";
+let extracted = sentence.slice(9, -5);
+
+console.log(extracted);
