8e2ec961e35f079ec083260e0b9d7a207083c2c4
Jonathan Fitzsimons <Jonathan.sfitzsimons@outlook.com>
2026-03-18 12:21:48 +1300

    Comment

---
+++ Var-DataType/Testing sheet.js
@@ -0,0 +1,13 @@
+const organization = "freeCodeCamp";
+console.log(organization.indexOf("c"));
+
+console.log("JavaScript".includes("Script"))
+
+console.log("JavaScript".slice(4))
+
+//const result = 3 + "19";
+const result = "6" - 4;
+console.log(result)
+
+const tnf = true + false;
+console.log(tnf)
